7d5e93e959f51243ac11fe2da5de0e9b07143427
AngrySoundTech <angrysoundtech@gmail.com>
2018-06-16 20:52:20 -0400

    Create and use BlockPos.distanceTo extension for volume calculation

--- src/main/kotlin/com/codingforcookies/betterrecords/util/VolumeCalcUtil.kt
+++ src/main/kotlin/com/codingforcookies/betterrecords/util/VolumeCalcUtil.kt
@@ -1,47 +1,56 @@
 package com.codingforcookies.betterrecords.util
 
 import com.codingforcookies.betterrecords.api.wire.IRecordWireHome
+import com.codingforcookies.betterrecords.block.tile.TileSpeaker
+import com.codingforcookies.betterrecords.extensions.distanceTo
 import net.minecraft.client.Minecraft
 import net.minecraft.util.SoundCategory
 import net.minecraft.util.math.BlockPos
 
+/**
+ *
+ */
 fun getVolumeForPlayerFromBlock(pos: BlockPos): Float {
     val player = Minecraft.getMinecraft().player
     val world = Minecraft.getMinecraft().world
 
-    val te = world.getTileEntity(pos)
+    // The record player or Radio.
+    // If it isn't one of those, we return a volume of zero.
+    val te = world.getTileEntity(pos) as? IRecordWireHome ?: return 0F
 
-    (te as? IRecordWireHome)?.let {
+    var distance = player.position.distanceTo(pos)
 
-        var dist = Math.abs(Math.sqrt(Math.pow(player.posX - pos.x, 2.toDouble()) +
-                Math.pow(player.posY - pos.y, 2.toDouble()) +
-                Math.pow(player.posZ - pos.z, 2.toDouble())))
+    // If there are any speakers closer to the player, we calculate the volume from that
+    te.connections.forEach {
+        val pos = BlockPos(it.x2, it.y2, it.z2)
 
-        for (rc in te.connections) {
-            val d = Math.abs(Math.sqrt(Math.pow(player.posX - rc.x2, 2.toDouble()) +
-                    Math.pow(player.posY - rc.y2, 2.toDouble()) +
-                    Math.pow(player.posZ - rc.z2, 2.toDouble())))
-            if (d < dist) {
-                dist = d
-            }
+        // If the tile isn't a speaker, we don't care about it
+        if (world.getTileEntity(pos) !is TileSpeaker) {
+            return@forEach
         }
 
-        return if (dist > te.songRadius + 10F) {
-            -80F
-        } else {
-            val volume = dist * (50F / te.songRadius /
-                    (Minecraft.getMinecraft().gameSettings.getSoundLevel(SoundCategory.MASTER) *
-                            Minecraft.getMinecraft().gameSettings.getSoundLevel(SoundCategory.RECORDS)))
+        val d = player.position.distanceTo(pos)
 
-            if (volume > 80F) {
-                -80F
-            } else {
-                0F - volume.toFloat()
-            }
+        // If it's closer to the player, we want to use that distance instead for our volume calculation
+        if (d < distance) {
+            distance = d
         }
-
-
     }
 
-    return 0F
+    // Calculate volume from whatever block was selected
+    return if (distance > te.songRadius + 10F) {
+        // If the player is outside of the radius of the player, we don't want them to hear it
+        -80F
+    } else {
+        // Take into account the user's settings
+        val volume = distance * (50F / te.songRadius /
+                (Minecraft.getMinecraft().gameSettings.getSoundLevel(SoundCategory.MASTER) *
+                        Minecraft.getMinecraft().gameSettings.getSoundLevel(SoundCategory.RECORDS)))
+
+        if (volume > 80F) {
+            -80F
+        } else {
+            0F - volume
+        }
+    }
 }
