7bcdb093d17beb841feb93b867d69f644ed982c5
AngrySoundTech <angrysoundtech@gmail.com>
2018-07-29 10:35:17 -0400

    Update SoundEventHandler to lower priorities and use distanceTo

--- src/main/kotlin/com/codingforcookies/betterrecords/client/handler/SoundEventHandler.kt
+++ src/main/kotlin/com/codingforcookies/betterrecords/client/handler/SoundEventHandler.kt
@@ -5,39 +5,38 @@ import com.codingforcookies.betterrecords.api.event.RadioInsertEvent
 import com.codingforcookies.betterrecords.api.event.RecordInsertEvent
 import com.codingforcookies.betterrecords.api.event.SoundStopEvent
 import com.codingforcookies.betterrecords.client.sound.SoundManager
+import com.codingforcookies.betterrecords.extensions.distanceTo
 import net.minecraft.client.Minecraft
 import net.minecraftforge.fml.common.Mod
+import net.minecraftforge.fml.common.eventhandler.EventPriority
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 import net.minecraftforge.fml.relauncher.Side
 
 @Mod.EventBusSubscriber(modid = ID, value = [Side.CLIENT])
 object SoundEventHandler {
 
-    @JvmStatic
-    @SubscribeEvent
+    @SubscribeEvent(priority = EventPriority.LOWEST)
     fun onRecordInserted(event: RecordInsertEvent) {
         val (pos, dimension, playRadius, sounds, repeat) = event
         val player = Minecraft.getMinecraft().player
 
-        if (playRadius > 100000 || Math.abs(Math.sqrt(Math.pow(player.posX - pos.x, 2.0) + Math.pow(player.posY - pos.y, 2.0) + Math.pow(player.posZ - pos.z, 2.0))).toFloat() < playRadius) {
+        if (playRadius > 100000 || player.position.distanceTo(pos) < playRadius) {
             SoundManager.queueSongsAt(pos, dimension, sounds, repeat = repeat )
         }
 
     }
 
-    @JvmStatic
-    @SubscribeEvent
+    @SubscribeEvent(priority = EventPriority.LOWEST)
     fun onRadioInserted(event: RadioInsertEvent) {
         val (pos, dimension, playRadius, sound) = event
         val player = Minecraft.getMinecraft().player
 
-        if (playRadius > 100000 || Math.abs(Math.sqrt(Math.pow(player.posX - pos.x, 2.0) + Math.pow(player.posY - pos.y, 2.0) + Math.pow(player.posZ - pos.z, 2.0))).toFloat() < playRadius) {
+        if (playRadius > 100000 || player.position.distanceTo(pos) < playRadius) {
             SoundManager.queueStreamAt(pos, dimension, sound)
         }
     }
 
-    @JvmStatic
-    @SubscribeEvent
+    @SubscribeEvent(priority = EventPriority.LOWEST)
     fun onSoundStopped(event: SoundStopEvent) {
         val (pos, dimension) = event
 
