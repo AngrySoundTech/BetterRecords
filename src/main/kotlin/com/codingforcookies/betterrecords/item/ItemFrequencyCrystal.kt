db52255af40baf17da8b39785bce5282311c4388
AngrySoundTech <angrysoundtech@gmail.com>
2018-11-15 19:53:43 -0500

    Fix display name of Frequency Crystals

--- src/main/kotlin/com/codingforcookies/betterrecords/item/ItemFrequencyCrystal.kt
+++ src/main/kotlin/com/codingforcookies/betterrecords/item/ItemFrequencyCrystal.kt
@@ -14,10 +14,20 @@ class ItemFrequencyCrystal(name: String) : ModItem(name), ISoundHolder, IColorab
         maxStackSize = 1
     }
 
-    override fun getItemStackDisplayName(stack: ItemStack) =
-            if (stack.hasTagCompound() && stack.tagCompound!!.hasKey("local")) {
-                stack.tagCompound!!.getString("local")
-            } else {
-                ServerI18n.translateToLocal("$unlocalizedName.name")
-            }
+    override fun getUnlocalizedName(stack: ItemStack): String {
+        // if there's a song tuned to it, it will be set in [getItemStackDisplayName]
+        return "item.betterrecords:frequencycrystal.blank"
+    }
+
+    override fun getItemStackDisplayName(stack: ItemStack): String {
+        val sounds = getSounds(stack)
+
+        // Use the name of the sound on the crystal if it is tuned
+        if (sounds.count() == 1) {
+            return sounds.first().name
+        }
+
+        // If it has no sounds, we fall back on localizing it.
+        return super.getItemStackDisplayName(stack)
+    }
 }
