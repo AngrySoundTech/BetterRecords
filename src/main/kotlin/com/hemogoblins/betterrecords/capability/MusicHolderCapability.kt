81638b9c458aa0d377ec093e05f84cb56fce8d1c
Azulaloi Champagne <azulaloichampagne@gmail.com>
2024-01-10 03:08:48 -0500

    Create MusicHolder capability

---
+++ src/main/kotlin/com/hemogoblins/betterrecords/capability/MusicHolderCapability.kt
@@ -0,0 +1,46 @@
+package com.hemogoblins.betterrecords.capability
+
+import com.hemogoblins.betterrecords.api.capability.IMusicHolder
+import net.minecraft.core.Direction
+import net.minecraft.nbt.CompoundTag
+import net.minecraftforge.common.capabilities.Capability
+import net.minecraftforge.common.capabilities.ICapabilitySerializable
+import net.minecraftforge.common.util.INBTSerializable
+import net.minecraftforge.common.util.LazyOptional
+
+class MusicHolderCapability: IMusicHolder, INBTSerializable<CompoundTag> {
+
+    override var songName = "DEFAULT"
+
+    override fun serializeNBT():CompoundTag {
+        val tag = CompoundTag()
+        tag.putString("songName", songName)
+        return tag
+    }
+
+    override fun deserializeNBT(tag: CompoundTag) {
+        songName = tag.getString("songName")
+    }
+
+    class Provider : ICapabilityCompoundable, ICapabilitySerializable<CompoundTag> {
+        override val key = "musicHolder"
+        private val musicHolder = MusicHolderCapability()
+
+
+        override fun <T : Any?> getCapability(cap: Capability<T>, side: Direction?): LazyOptional<T> {
+            return when (cap) {
+                ModCapabilities.MUSIC_HOLDER_CAPABILITY -> LazyOptional.of { musicHolder } as LazyOptional<T>
+                else -> LazyOptional.empty()
+            }
+        }
+
+        override fun serializeNBT(): CompoundTag {
+            return musicHolder.serializeNBT()
+        }
+
+        override fun deserializeNBT(tag: CompoundTag) {
+            return musicHolder.deserializeNBT(tag)
+        }
+    }
+
+}
