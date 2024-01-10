a6975d4dbd3c71dfd9bb0a3c08beb81cdf81698e
Azulaloi Champagne <azulaloichampagne@gmail.com>
2024-01-10 06:44:35 -0500

    Add config scaffold

--- src/main/kotlin/com/hemogoblins/betterrecords/BetterRecords.kt
+++ src/main/kotlin/com/hemogoblins/betterrecords/BetterRecords.kt
@@ -3,7 +3,9 @@ package com.hemogoblins.betterrecords
 import com.hemogoblins.betterrecords.block.ModBlocks
 import com.hemogoblins.betterrecords.capability.ModCapabilities
 import com.hemogoblins.betterrecords.item.ModItems
+import net.minecraftforge.fml.ModLoadingContext
 import net.minecraftforge.fml.common.Mod
+import net.minecraftforge.fml.config.ModConfig
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent
 import net.minecraftforge.fml.event.lifecycle.FMLDedicatedServerSetupEvent
 import org.apache.logging.log4j.LogManager
@@ -24,6 +26,10 @@ object BetterRecords {
         ModBlocks.register(MOD_BUS)
         ModItems.register(MOD_BUS)
         ModCapabilities.register(MOD_BUS)
+
+        ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, BRConfig.CLIENT_SPEC)
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, BRConfig.COMMON_SPEC)
+        ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, BRConfig.SERVER_SPEC)
 
         runForDist(
                 clientTarget = {
